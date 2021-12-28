37e0ffcba1413c5adf77b491ef13eca325c25bc3
maxime.richaudeau <maxime.richaudeau1@gmail.com>
2021-12-28 21:02:29 +0100

    Add email in search_iphone.json to enable new_client_registration and sending messages only to the concerned client

--- IOs/iphone.py
+++ IOs/iphone.py
@@ -1,11 +1,27 @@
-import encodings
 import json
 
 
-def read_json():
+def read_iphone_searched():
     """
     From the searched_iphone file
     :return: a list of iphone
     """
     with open("C:\\Users\\maxim\\PYTHON\\Appli_alerte\\searched_iphone.json", encoding='UTF_8') as file_of_searched_iphones:
         return json.loads(file_of_searched_iphones.read())
+
+
+def append_new_iphone(email, model, stockage, etat, couleur, prix):
+    iphone_searched = {"email": email,
+                       "model": model,
+                       "stockage": int(stockage),
+                       "etat": etat,
+                       "couleur": couleur,
+                       "prix": int(prix)}
+
+    iphone_tableau = read_iphone_searched()
+    iphone_tableau.append(iphone_searched)
+    print(iphone_tableau)
+
+    with open("C:\\Users\\maxim\\PYTHON\\Appli_alerte\\searched_iphone.json", mode='w', encoding='UTF_8') as f:
+        f.write(json.dumps(iphone_tableau, ensure_ascii=False))
+
--- alerts/find_potential_iphones.py
+++ alerts/find_potential_iphones.py
@@ -1,4 +1,4 @@
-from IOs.iphone import read_json as find_iphones
+from IOs.iphone import read_iphone_searched as find_iphones
 import time
 import pandas as pd
 import glob
@@ -8,20 +8,26 @@ def search_in_db():
 
     searched_iphone = find_iphones()
     timestamp = time.strptime(time.strftime("%Y/%m/%d %H:%M:%S"), "%Y/%m/%d %H:%M:%S")
-    path = f"C:\\Users\\maxim\\PYTHON\\Appli_alerte\\data\\{timestamp.tm_year}\\{timestamp.tm_mon}\\24\\11"
+    path = f"C:\\Users\\maxim\\PYTHON\\Appli_alerte\\data\\{timestamp.tm_year}\\{timestamp.tm_mon}\\{timestamp.tm_mday}"
     #print(searched_iphone)
     #print(path)
-    csv_path = glob.glob(f"{path}\\*.csv")
+    csv_path = glob.glob(f"{path}\\*\\*.csv")
     #print(csv_path)
-    iphone_data = pd.concat([pd.read_csv(csv_file) for csv_file in csv_path ], ignore_index=True)
-    iphone_data.model = list(map(lambda x: str.lower(x).replace(" ", ""), iphone_data['model'].tolist()))
+    # iphone_data = pd.concat([pd.read_csv(csv_file) for csv_file in csv_path ], ignore_index=True)
+    # iphone_data.model = list(map(lambda x: str.lower(x).replace(" ", ""), iphone_data['model'].tolist()))
     iphone_found = {}
     for tel in searched_iphone:
+        iphone_data = pd.concat([pd.read_csv(csv_file) for csv_file in csv_path], ignore_index=True)
+        iphone_data.model = list(map(lambda x: str.lower(x).replace(" ", ""), iphone_data['model'].tolist()))
         iphone_data = iphone_data[iphone_data.model == (str.lower(tel["model"]).replace(" ", ""))]
         iphone_data = iphone_data[iphone_data.stockage == tel["stockage"]]
         iphone_data = iphone_data[iphone_data.prix <= tel["prix"]]
         iphone_data = iphone_data[iphone_data.etat == tel["etat"]]
-        iphone_data = iphone_data[iphone_data.couleur.isin(tel["couleur"])]
-        iphone_found[tel["model"]] = iphone_data.values.tolist()
+        iphone_data = iphone_data[iphone_data.couleur == tel["couleur"]]
+        try:
+            iphone_found[tel["model"]].append(iphone_data.values.tolist())
+        except:
+            iphone_found[tel["model"]] = []
+            iphone_found[tel["model"]].append(iphone_data.values.tolist())
     #print(iphone_found)
     return iphone_found
