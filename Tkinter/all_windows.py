4238a7afacc082d2c30876e400ce92f8912eadc7
issa-qista <issa@qista.com>
2022-01-07 19:30:59 +0100

    Add some explaination about different windows

--- Tkinter/all_windows.py
+++ Tkinter/all_windows.py
@@ -10,8 +10,16 @@ from scrap.scrapping import get_all_iphone_div, get_iphone_all_details
 def scrapping_window(root):
     child_scrapping = tk.Toplevel(root)
     child_scrapping.geometry("300x250")
-    child_scrapping.grid()
     child_scrapping.title("Scrapping Window")
+    text = tk.Text(child_scrapping)
+    text.insert(tk.INSERT, "This part is used to scrap BackMarket\n"
+                           "The process is an hour long.\n\n"
+                           "If you want to update the database\n"
+                           "Please click on Send a Scrap\n\n"
+                           "Else Quit to go back to main menu."
+                )
+    text.insert(tk.END, "")
+    text.pack()
     def scrapping_function():
         all_urls = read_urls()
         print(all_urls)
@@ -29,42 +37,64 @@ def scrapping_window(root):
                         pass
             close_file(file)
     # Button to send email
-    tk.Button(child_scrapping, text="Send a scrap on BackMarket", command=scrapping_function).grid(column=2, row=1)
+    tk.Button(child_scrapping, text="Send a scrap on BackMarket", command=scrapping_function).place(x=0, y=220)
     # quit button
-    tk.Button(child_scrapping, text="Quit", command=child_scrapping.destroy).grid(column=1, row=2)
+    tk.Button(child_scrapping, text="Quit", command=child_scrapping.destroy).place(x=250, y=220)
 
 
 def email_window(root):
     child_email = tk.Toplevel(root)
     child_email.geometry("300x250")
-    child_email.grid()
     child_email.title("Email Window")
-    tk.Label(child_email, text="Your email adress :").grid(row=0)
+
+    text = tk.Text(child_email)
+    text.insert(tk.INSERT, "This part is used to send email\n"
+                           "to user who has already registered.\n\n"
+                           "If you want to be advise by email if\n"
+                           "we have found the corresponding one.\n"
+                           "Please fill the text box with the \n"
+                           "email you chose at registration\n"
+                           "and click on Send an Email.\n\n"
+                           "Else Quit to go back to main menu."
+                )
+    text.insert(tk.END, "")
+    text.pack()
+
+    tk.Label(child_email, text="Your email adress :").place(x=0, y=180)
     email = tk.Entry(child_email, borderwidth=2)
-    email.grid(row=0, column=1)
+    email.place(x=150, y=180)
 
     def email_sender():
         user_email = email.get()
         send_email(user_email, search_in_db())
 
     # Button to send email
-    tk.Button(child_email, text="Send an email", command=email_sender).grid(column=2, row=2)
+    tk.Button(child_email, text="Send an email", command=email_sender).place(x=0, y=220)
     # quit button
-    tk.Button(child_email, text="Quit", command=child_email.destroy).grid(column=1, row=2)
+    tk.Button(child_email, text="Quit", command=child_email.destroy).place(x=250, y=220)
 
 
 def register_window(root):
     child_register = tk.Toplevel(root)
-    child_register.geometry("300x250")
+    child_register.geometry("300x300")
     child_register.grid()
-    child_register.title("New Child Window")
+    child_register.title("Registration Window")
 
-    tk.Label(child_register, text="Your email adress :").grid(row=0)
-    tk.Label(child_register, text="Iphone model :").grid(row=1)
-    tk.Label(child_register, text="Iphone Stockage :").grid(row=2)
-    tk.Label(child_register, text="Iphone Renoved State :").grid(row=3)
-    tk.Label(child_register, text="Iphone Color :").grid(row=4)
-    tk.Label(child_register, text="Maximum Price Desired : ").grid(row=5)
+    text = tk.Text(child_register)
+    text.insert(tk.INSERT, "This part is used to register\n"
+                           "all details we need to find your\n"
+                           "desired Iphone and send the alert\n"
+                           "by email!\n\n"
+                )
+    text.insert(tk.END, "")
+    text.pack()
+
+    tk.Label(child_register, text="Your email adress :").place(x=0, y=80)
+    tk.Label(child_register, text="Iphone model :").place(x=0, y=110)
+    tk.Label(child_register, text="Iphone Stockage :").place(x=0, y=140)
+    tk.Label(child_register, text="Iphone Renoved State :").place(x=0, y=170)
+    tk.Label(child_register, text="Iphone Color :").place(x=0, y=200)
+    tk.Label(child_register, text="Maximum Price Desired : ").place(x=0, y=230)
 
     liste_model = ["iPhone 12", "iPhone 12 mini", "iPhone 12 Pro", "iPhone 12 Pro Max", "iPhone 11",
                    "iPhone 11 Pro", "iPhone 11 Pro Max"]
@@ -90,12 +120,12 @@ def register_window(root):
     iphone_color = tk.OptionMenu(child_register, color_var, *liste_color)
     iphone_price = tk.Entry(child_register, borderwidth=2)
 
-    email.grid(row=0, column=1)
-    iphone_model.grid(row=1, column=1)
-    iphone_stockage.grid(row=2, column=1)
-    iphone_state.grid(row=3, column=1)
-    iphone_color.grid(row=4, column=1)
-    iphone_price.grid(row=5, column=1)
+    email.place(x=150, y=80)
+    iphone_model.place(x=165, y=110)
+    iphone_stockage.place(x=185, y=140)
+    iphone_state.place(x=165, y=170)
+    iphone_color.place(x=185, y=200)
+    iphone_price.place(x=150, y=230)
 
     def register_user():
         user_email = email.get()
@@ -107,5 +137,5 @@ def register_window(root):
 
         append_new_iphone(user_email, model, stockage, state, color, price)
 
-    tk.Button(child_register, text="Save Profil", command=register_user).grid(column=0, row=6)
-    tk.Button(child_register, text="Quit", command=child_register.destroy).grid(column=1, row=6)
+    tk.Button(child_register, text="Save Profil", command=register_user).place(x=0, y=260)
+    tk.Button(child_register, text="Quit", command=child_register.destroy).place(x=250, y=260)
--- main.py
+++ main.py
@@ -5,14 +5,25 @@ from Tkinter.all_windows import scrapping_window, email_window, register_window
 
 if __name__ == '__main__':
     root = tk.Tk()
+    root.geometry("300x250")
+    text = tk.Text(root)
+    text.insert(tk.INSERT, "This application is useful to be\n"
+                           "alerted by email if we find an\n"
+                           "Iphone which you would like to buy.\n\n"
+                           "To register your profil please click\n"
+                           "Register. If you already have an\n"
+                           "account please click send email.\n\n"
+                           "Else quit or update DB (1h aprox)")
+    text.insert(tk.END, "")
+    text.pack()
     # Button to update data
-    tk.Button(root, text="Mise a jour des données", command=partial(scrapping_window, root)).grid(column=3, row=0)
+    tk.Button(root, text="Update database", command=partial(scrapping_window, root)).place(x=0, y=220)
     # Button to send email
-    tk.Button(root, text="Send an email", command=partial(email_window, root)).grid(column=3, row=1)
+    tk.Button(root, text="Send an email",  command=partial(email_window, root)).place(x=0, y=190)
     # Button to register a new request
-    tk.Button(root, text="Register", command=partial(register_window, root)).grid(column=1, row=1)
+    tk.Button(root, text="Register", command=partial(register_window, root)).place(x=0, y=160)
     # quit button
-    tk.Button(root, text="Quit", command=root.destroy).grid(column=1, row=2)
+    tk.Button(root, text="Quit", command=root.destroy).place(x=250, y=220)
     root.mainloop()
 
 
